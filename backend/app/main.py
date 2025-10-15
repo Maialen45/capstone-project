bcde8518a9a334bc638572f86135d440520360ec
Maialen45 <maialenes@gmail.com>
2025-10-15 13:19:27 +0200

    solved error: cookies managing for mobiles

--- backend/app/main.py
+++ backend/app/main.py
@@ -26,7 +26,7 @@ def create_app():
     app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token_cookie'
     app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(minutes=60)
     app.config["JWT_REFRESH_TOKEN_EXPIRES"] = timedelta(days=7)
-    app.config['JWT_COOKIE_CSRF_PROTECT'] = True  
+    app.config['JWT_COOKIE_CSRF_PROTECT'] = False  
     app.config['JWT_COOKIE_SECURE'] = True
     app.config['JWT_COOKIE_SAMESITE'] = 'None'
 
